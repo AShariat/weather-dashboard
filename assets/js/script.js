aa612f7c5048e9bd7699bb1abfe4c1d2a4294e91
Amir Shariat <amirshariat1987@gmail.com>
2022-01-31 02:01:50 -0700

    added forecast section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
+const now = moment().format('L');
 const key = '32ed05888dd11ce89460365fde5e625e';
 const search = document.querySelector('#input');
 const display = document.querySelector('#display');
-const button = document.querySelector('button');
-const cityName = document.querySelector('#cityName');
+const icon = document.querySelector('#icon');
 function getWeather(lat, lon) {
   fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&exclude=hourly,minutely&units=imperial&lang=en&appid='+key)
   .then(response => {
@@ -11,9 +11,11 @@ function getWeather(lat, lon) {
   .then(data => {
     console.log(data);
     console.log(data.current);
-    document.querySelector('#currentTemp').textContent = "Temp: " + data.current.temp;
-    document.querySelector('#currentWind').textContent = "Wind: " + data.current.temp;
-    document.querySelector('#currentHumidity').textContent = "Humidity: " + data.current.temp;
+    var iconCode = data.current.weather[0].icon;
+    icon.setAttribute('src', 'http://openweathermap.org/img/wn/' + iconCode + '@2x.png');
+    document.querySelector('#currentTemp').textContent = data.current.temp + " F";
+    document.querySelector('#currentWind').textContent = data.current.wind_speed + " MPH";
+    document.querySelector('#currentHumidity').textContent = data.current.humidity + " %";
     document.querySelector('#uv').textContent = data.current.uvi;
   })
   .catch(err => {
@@ -32,15 +34,15 @@ function getLatLon(value) {
     return response.json();
     })
     .then(data => {
-      console.log(data[0]);
-      console.log(data[0].name);
-      document.querySelector('#cityName').textContent = data[0].name;
+      document.querySelector('#cityName').textContent = " " + data[0].name +", " + data[0].region;
+      document.querySelector('#currentDate').textContent = now;
+      console.log(now);
       getWeather(data[0].lat, data[0].lon);
     })
     .catch(err => {
       console.error(err);
     })
 };
-button.addEventListener('click', function() {
+document.querySelector('button').addEventListener('click', function() {
   getLatLon(search.value);
-})
+});
